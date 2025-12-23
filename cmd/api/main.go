07cd17859ade1700974e2e6dbf0a64ff60fcd164
Utkarsh Kumar Singh <utkarsh.isa.dev@gmail.com>
2025-12-23 23:32:29 +0530

    [added]: routes for the api

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/utkarshkrsingh/restapi/internal/routes"
+)
+
+func main() {
+	router := mux.NewRouter()
+	routes.HandleRecordRoutes(router)
+
+	log.Println("server starting at port :8080")
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatalf("server failed: %v\n", err)
+	}
+}
